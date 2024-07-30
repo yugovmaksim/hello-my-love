632767b49818b50a70e1f920bcac187c9c2cb49e
Yugov Maksim <yugov_maksim89@mail.ru>
2024-07-31 04:49:37 +0500

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(`button`);
+const output = document.querySelector(`.output`);
+
+button.addEventListener(`click`, function () {
+  output.innerHTML = `Дарю тебе от чистого сердца, и искренней любовью &#129505`;
+});
